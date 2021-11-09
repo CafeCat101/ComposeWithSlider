712569fe40ea02a7e1034be02e118e1f59023de5
Leonore Yardimli <leonore1982@gmail.com>
2021-11-10 01:51:09 +0800

    Initial Commit

---
+++ ComposeWithSlider/ComposeWithSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposeWithSliderApp.swift
+//  ComposeWithSlider
+//
+//  Created by Leonore Yardimli on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct ComposeWithSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
